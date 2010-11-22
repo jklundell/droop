74280c963c77bafbc9a728ea0fa5f822d6d9c01b
Jonathan Lundell <jlundell@pobox.com>
2010-11-22 13:19:37 -0800

    rely on PATH for nosetests

--- test/nose.sh
+++ test/nose.sh
@@ -2,12 +2,14 @@
 #
 #  run nose with optional coverage
 #
+#  http://pypi.python.org/pypi/nose
+#  http://pypi.python.org/pypi/coverage
 #  http://somethingaboutorange.com/mrl/projects/nose/0.11.3/testing.html
 #  http://somethingaboutorange.com/mrl/projects/nose/0.11.3/plugins/cover.html
 #  http://nedbatchelder.com/code/coverage/
 #  http://bitbucket.org/ned/coveragepy/issues
 #
-NOSETESTS=/usr/local/bin/nosetests
+NOSETESTS=nosetests
 if [ "$1" = "cover" ]; then
 	$NOSETESTS --with-coverage --cover-package=droop --cover-erase 
 elif [ "$1" = "cover" ]; then
