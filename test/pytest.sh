ad4f874ca9cddd4d8db80a1c912455374d67620f
Jonathan Lundell <jlundell@pobox.com>
2018-07-09 07:12:10 -0500

    nose -> pytest

--- test/pytest.sh
+++ test/pytest.sh
@@ -1,22 +1,11 @@
 #!/bin/sh
 #
-#  run nose with optional coverage
+#  run pytest with optional coverage
 #
-#  http://pypi.python.org/pypi/nose
-#  http://pypi.python.org/pypi/coverage
-#  http://somethingaboutorange.com/mrl/projects/nose/0.11.3/testing.html
-#  http://somethingaboutorange.com/mrl/projects/nose/0.11.3/plugins/cover.html
-#  http://nedbatchelder.com/code/coverage/
-#  http://bitbucket.org/ned/coveragepy/issues
-#
-#NOSETESTS=/usr/local/bin/nosetests # nose
-#NOSETESTS=nose2
-NOSETESTS=pytest
+PYTEST=pytest
 
 if [ "$1" = "cover" ]; then
-	$NOSETESTS --process-timeout=30 --with-coverage --cover-package=droop --cover-erase
-elif [ "$1" = "coverx" ]; then
-	$NOSETESTS --processes=4 --process-timeout=30 --with-coverage --cover-package=droop --cover-erase
+	$PYTEST --cov-report term-missing --cov=droop
 else
-	$NOSETESTS --processes=4 --process-timeout=30
+	$PYTEST
 fi
