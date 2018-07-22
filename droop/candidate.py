52d9d0fdaa3852a3a249158a7ddfd530a08a2e2d
Jonathan Lundell <jlundell@pobox.com>
2018-07-22 14:39:39 -0500

    move Candidate & Candidates classes to their own files

---
+++ droop/candidate.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+'''
+Generic Election Support
+
+Copyright 2010 by Jonathan Lundell
+
+This file is part of Droop.
+
+    Droop is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Droop is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Droop.  If not, see <http://www.gnu.org/licenses/>.
+
+Top-level structure:
+
+  A driver program (for example Droop.py, the CLI)
+    1. creates an ElectionProfile from a ballot file,
+    2. imports a Rule,
+    3. creates an Election(Rule, ElectionProfile, options),
+    4. counts the election with Election.count(), and
+    5. generates a report with Election.report().
+
+  The options are used to override default Rule parameters, such as arithmetic.
+'''
+
+from __future__ import absolute_import
+
+class Candidate(object):
+    '''
+    a candidate, with state
+    '''
+    def __init__(self, E, cid, ballotOrder, tieOrder, cname, cnick, isWithdrawn):
+        "new candidate"
+        # immutable properties
+        self.E = E
+        self.cid = cid              # candidate id
+        self.order = ballotOrder    # ballot order
+        self.tieOrder = tieOrder    # tie-breaking order
+        self.name = cname           # candidate name
+        self.nick = str(cid) if cnick is None else str(cnick)
+        # mutable properties
+        self.state = 'withdrawn' if isWithdrawn else 'hopeful'  # withdrawn, hopeful, elected, etc
+        if E is None:
+            self.vote = None        # in support of unit tests
+        else:
+            self.vote = E.V0        # current vote total
+        self.kf = None              # current keep factor (meek)
+        self.quotient = None        # current quotient (qpq)
+        self.pending = None         # surplus-transfer pending (wigm)
+
+    def as_dict(self, ro=False, rw=False):
+        "return as a dict suitable for JSON encoding"
+        cdict = dict()
+        if ro:
+            cdict['cid'] = self.cid
+            cdict['ballot_order'] = self.order
+            cdict['tie_order'] = self.tieOrder
+            cdict['name'] = self.name
+            cdict['nick'] = self.nick
+        if rw:
+            cdict['state'] = self.state
+            cdict['code'] = self.code()
+            if self.state != 'withdrawn':
+                cdict['vote'] = self.vote
+                if self.kf is not None:
+                    cdict['kf'] = self.kf
+                if self.quotient is not None:
+                    cdict['quotient'] = self.quotient
+                if self.pending is not None:
+                    cdict['pending'] = self.pending
+        return cdict
+
+    @property
+    def surplus(self):
+        "return candidate's current surplus vote"
+        s = self.vote - self.E.quota
+        return self.E.V0 if s < self.E.V0 else s
+
+    def elect(self, msg=None, pending=False):
+        '''
+        Meek, QPQ: elect a candidate
+        WIGM: elect a candidate, optionally pending surplus transfer
+        '''
+        self.state = 'elected'
+        if msg is None:
+            msg = 'Elect, transfer pending' if pending else 'Elect'
+        self.pending = pending
+        self.E.logAction('elect', "%s: %s" % (msg, self.name))
+
+    def unpend(self, msg=None):
+        '''
+        WIGM: clear the transfer-pending flag
+        '''
+        assert self.state == 'elected'
+        assert self.pending
+        self.pending = False
+        if msg:
+            self.E.logAction('unpend', "%s: %s" % (msg, self.name))
+
+    def unelect(self):
+        "QPQ: unelect a candidate (qpq restart)"
+        self.state = 'hopeful'
+
+    def defeat(self, msg='Defeat'):
+        "defeat a candidate"
+        self.state = 'defeated'
+        self.E.logAction('defeat', "%s: %s" % (msg, self.name))
+
+    def code(self):
+        "return a one-letter state code for a candidate"
+        if self.state == 'withdrawn':
+            return 'W'
+        if self.state == 'hopeful':
+            return 'H'
+        if self.state == 'elected':
+            if self.E.rule.method == 'wigm' and self.pending:
+                return 'e'
+            return 'E'
+        if self.state == 'defeated':
+            return 'D'
+        return '?'  # pragma: no cover
+
+    def __str__(self):
+        "use candidate name as the string representation"
+        return self.name
+
+    def __hash__(self):
+        "use candidate id as the candidate hash"
+        return self.cid
+
+    def __eq__(self, other):
+        "test for equality of cid"
+        if isinstance(other, int):
+            return self.cid == other
+        if isinstance(other, str):
+            return str(self.cid) == other
+        if other is None:
+            return False
+        return self.cid == other.cid
---
+++ droop/candidates.py
@@ -0,0 +1,164 @@
+# -*- coding: utf-8 -*-
+'''
+Generic Election Support
+
+Copyright 2010 by Jonathan Lundell
+
+This file is part of Droop.
+
+    Droop is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Droop is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Droop.  If not, see <http://www.gnu.org/licenses/>.
+
+Top-level structure:
+
+  A driver program (for example Droop.py, the CLI)
+    1. creates an ElectionProfile from a ballot file,
+    2. imports a Rule,
+    3. creates an Election(Rule, ElectionProfile, options),
+    4. counts the election with Election.count(), and
+    5. generates a report with Election.report().
+
+  The options are used to override default Rule parameters, such as arithmetic.
+'''
+
+from __future__ import absolute_import
+import copy
+
+class Candidates(set):
+    '''
+    all candidates
+    '''
+    def __init__(self, E=None):
+        "new Candidates"
+        super(Candidates, self).__init__()
+        self.E = E              # Election
+        self._byCid = dict()    # side table: cid -> Candidate
+
+    def copy(self):
+        "return a copy of ourself"
+        C = Candidates(self.E)
+        for c in self:
+            super(Candidates, C).add(copy.copy(c))
+        return C
+
+    def add(self, c):
+        "add a candidate"
+        self._byCid[c.cid] = c  # side table for lookup by candidate ID
+        super(Candidates, self).add(c)
+        if self.E is not None:  # accommodate unit test
+            if c.state == 'withdrawn':
+                self.E.log("Add withdrawn: %s" % c.name)
+            else:
+                self.E.log("Add eligible: %s" % c.name)
+
+    def byCid(self, cid):
+        "look up a candidate by candidate ID"
+        return self._byCid[cid]
+
+    def cidList(self, state='all'):
+        '''
+        return a list of CIDs, in ballot order
+
+        used for reporting
+        '''
+        return [c.cid for c in self.select(state, order='ballot')]
+
+    def cDict(self):
+        '''
+        return dict of candidate static info, keyed by CID
+        variant state is not included (see cState())
+
+        used for reporting
+        '''
+        cdict = dict()
+        for c in self.select('all'):
+            cdict[c.cid] = c.as_dict(ro=True)
+        return cdict
+
+    def cState(self):
+        '''
+        return a dict of candidate state, keyed by CID
+        invariant state is not included (see cDict())
+        withdrawn candidates have an abbreviated state (see c.as_dict())
+
+        used for reporting
+        '''
+        cstate = dict()
+        for c in self.select('all'):
+            cstate[c.cid] = c.as_dict(rw=True)
+        return cstate
+
+    @staticmethod
+    def byBallotOrder(candidates, reverse=False):
+        "sort a list of candidates by ballot order"
+        return sorted(candidates, key=lambda c: c.order, reverse=reverse)
+
+    @staticmethod
+    def byVote(candidates, reverse=False):
+        "sort a list of candidates by vote order"
+        return sorted(candidates, key=lambda c: (c.vote, c.order), reverse=reverse)
+
+    @staticmethod
+    def byTieOrder(candidates, reverse=False):
+        "sort a list of candidates by tie-break order"
+        return sorted(candidates, key=lambda c: c.tieOrder, reverse=reverse)
+
+    def select(self, state, order='none', reverse=False):
+        "select and return list of candidates with specified state, optionally in specified order"
+        if state == 'all':
+            candidates = self   # set of all
+        elif state == 'eligible':
+            candidates = [c for c in self if c.state != 'withdrawn']
+        elif state == 'pending':
+            candidates = [c for c in self if c.state == 'elected' and c.pending]
+        elif state == 'notpending':
+            candidates = [c for c in self if c.state == 'elected' and not c.pending]
+        else:
+            candidates = [c for c in self if c.state == state]  # list of all
+        if order == 'none':
+            return candidates
+        if order == 'ballot':
+            return self.byBallotOrder(candidates, reverse=reverse)
+        if order == 'tie':
+            return self.byTieOrder(candidates, reverse=reverse)
+        if order == 'vote':
+            return self.byVote(candidates, reverse=reverse)
+        raise ValueError('unknown candidate sort order: %s' % order)
+
+    def eligible(self, order='none', reverse=False):
+        "select and return list of eligible candidates, in specified order"
+        return self.select('eligible', order, reverse)
+
+    def withdrawn(self, order='none', reverse=False):
+        "select and return list of eligible candidates, in specified order"
+        return self.select('withdrawn', order, reverse)
+
+    def hopeful(self, order='none', reverse=False):
+        "select and return list of hopeful candidates, in specified order"
+        return self.select('hopeful', order, reverse)
+
+    def elected(self, order='none', reverse=False):
+        "select and return list of withdrawn candidates, in specified order"
+        return self.select('elected', order, reverse)
+
+    def defeated(self, order='none', reverse=False):
+        "select and return list of defeated candidates, in specified order"
+        return self.select('defeated', order, reverse)
+
+    def notpending(self, order='none', reverse=False):
+        "select and return list of elected and not pending candidates, in specified order"
+        return self.select('notpending', order, reverse)
+
+    def pending(self, order='none', reverse=False):
+        "select and return list of elected candidates pending transfer, in specified order"
+        return self.select('pending', order, reverse)
