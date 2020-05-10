128bb98faaf4f023bc8ec8db890b403a19385bc1
Jonathan Lundell <jlundell@pobox.com>
2020-05-10 10:04:04 -0500

    v0.13

--- droop/common.py
+++ droop/common.py
@@ -21,7 +21,7 @@ This file is part of Droop.
 '''
 
 droopName = 'droop'
-droopVersion = '0.12'
+droopVersion = '0.13'
 droopAuthor = 'Jonathan Lundell'
 droopAuthorEmail = 'jlundell@prfound.org'
 droopURL = 'http://prfound.org'
